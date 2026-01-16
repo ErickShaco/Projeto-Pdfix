311150830974941f8a199b908dbfdb38501ca7eb
MATEUS OLIVEIRA IRINEU <a95602912@rn.senac.local>
2026-01-16 19:18:32 -0300

    feat(BD): Criado Banco de dados

---
+++ backend/src/models/usuario.js
@@ -0,0 +1,71 @@
+import _sequelize from 'sequelize';
+const { Model, Sequelize } = _sequelize;
+
+export default class usuario extends Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    usuario_id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    nome: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: "usuario_email_key"
+    },
+    telefone: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    endereco: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    idade: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    linkedin_url: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    criado_em: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    atualizado_em: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, {
+    sequelize,
+    tableName: 'usuario',
+    schema: 'public',
+    timestamps: false,
+    indexes: [
+      {
+        name: "usuario_email_key",
+        unique: true,
+        fields: [
+          { name: "email" },
+        ]
+      },
+      {
+        name: "usuario_pkey",
+        unique: true,
+        fields: [
+          { name: "usuario_id" },
+        ]
+      },
+    ]
+  });
+  }
+}
